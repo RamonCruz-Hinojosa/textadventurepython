8239d3c3f493dd4393dcf0a0086c032d19264369
RamonCruz-Hinojosa <bairdwins@gmail.com>
2025-09-11 15:06:57 -0500

    L7 more print stuff

--- textadventure.py
+++ textadventure.py
@@ -1,9 +1,18 @@
 #print("Welcome to Fantasy Quest!")
 
-sword_damage = 10
-start_health = 100
-end_health = start_health - sword_damage
+#sword_damage = 10
+#start_health = 100
+#end_health = start_health - sword_damage
 
-print(f"Sam's health is: {start_health}")
-print(f"Sam takes {sword_damage} damage...")
-print(f"Sam's health is now: {end_health}")
+#print(f"Sam's health is: {start_health}")
+#print(f"Sam takes {sword_damage} damage...")
+#print(f"Sam's health is now: {end_health}")
+
+#print("Please move forward")
+
+#print(250 + 75)
+
+print("Jax: B-Kaw!")
+print("Hero: ...")
+print("Jax: Where are you off to this morning? Bkaw...")
+print("Hero: What does that mean?")
