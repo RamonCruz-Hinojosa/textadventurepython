8636fccb3f50fa3f389ee54122271c1adc480678
RamonCruz-Hinojosa <bairdwins@gmail.com>
2025-09-11 14:03:20 -0500

    basic print statements

--- textadventure.py
+++ textadventure.py
@@ -1,1 +1,9 @@
-print("Welcome to Fantasy Quest!")
+#print("Welcome to Fantasy Quest!")
+
+sword_damage = 10
+start_health = 100
+end_health = start_health - sword_damage
+
+print(f"Sam's health is: {start_health}")
+print(f"Sam takes {sword_damage} damage...")
+print(f"Sam's health is now: {end_health}")
