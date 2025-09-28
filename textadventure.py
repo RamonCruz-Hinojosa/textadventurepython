6e18e7b864a1afae76c85d3dcee7dfb6979991a2
RamonCruz-Hinojosa <bairdwins@gmail.com>
2025-09-28 03:09:44 -0500

    finished more basic syntax errors on L11

--- textadventure.py
+++ textadventure.py
@@ -12,7 +12,10 @@
 
 #print(250 + 75)
 
-print("Jax: B-Kaw!")
-print("Hero: ...")
-print("Jax: Where are you off to this morning? Bkaw...")
-print("Hero: What does that mean?")
+#print("Jax: B-Kaw!")
+#print("Hero: ...")
+#print("Jax: Where are you off to this morning? Bkaw...")
+#print("Hero: Where did an owl learn to speak??")
+
+#print("Welcome to Fantasy Quest!")
+
