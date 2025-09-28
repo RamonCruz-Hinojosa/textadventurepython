b249be3feb1548d825124b8f99b3ba1d1bfe4e53
RamonCruz-Hinojosa <bairdwins@gmail.com>
2025-09-28 03:27:21 -0500

    added more shop dialogue

--- textadventure.py
+++ textadventure.py
@@ -21,3 +21,8 @@
 
 #print("Starting up game server...")
 #print("local game server is listening on port 8080")
+
+#print((250 + 241 + 244 + 255) / 4)
+
+#print("Ah! Great choices...")
+#print("Is there anything else I can help you with?")
