8148e223d3e5016ca5fc5768087658b79d29ad95
RamonCruz-Hinojosa <bairdwins@gmail.com>
2025-09-29 09:30:01 -0500

    went through basic variable assignments stopped on L9

--- ch2.py
+++ ch2.py
@@ -1,1 +1,33 @@
 player_health = 1000
+#the best_sword variable holds the value of the best sword in the game !this line is just to learn comments
+
+"""
+posison_damage = -10
+armor_multiplier = 2
+
+player_health = 900
+
+print(player_health)
+
+player_health = 800
+
+print(player_health)
+
+player_health = 700
+
+print(player_health)
+
+player_health = 600
+
+print(player_health)
+
+armored_health = player_health * armor_multiplier 
+print(armored_health)
+
+player_poison_health = player_health + posison_damage
+print(player_poison_health)
+
+player_health = 100 this line was changed from "100" to 100
+
+player_has_magic = true this line was changed from "true" to true
+"""
