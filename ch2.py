e7a500084990bd86c8c84f6ea3006e0029096f60
RamonCruz-Hinojosa <bairdwins@gmail.com>
2025-10-02 04:13:49 -0500

    more basic variable stuff ended on L15

--- ch2.py
+++ ch2.py
@@ -30,4 +30,28 @@ print(player_poison_health)
 player_health = 100 this line was changed from "100" to 100
 
 player_has_magic = true this line was changed from "true" to true
+
+Lesson 9
+fix bug in the print statement
+name = "Yarl"
+age = 37
+race = "dwarf"
+
+print("NAME is a RACE who is AGE years old.")
+
+answer here: print(f"{name} is a {race} who is {age} years old.")
+
+lesson 10
+enemy = None
+print(enemy is None)
+
+lesson 14
+sentence_start = "You have "
+sentence_end = " health"
+
+player1_health = "1200"
+player2_health = "1100"
+
+print(sentence_start + player1_health + sentence_end)
+print(sentence_start + player2_health + sentence_end)
 """
