62e6e6656b96f795a5bf290396ec3402e033ea76
RamonCruz-Hinojosa <bairdwins@gmail.com>
2025-10-14 12:10:59 -0500

    end of ch2 onto ch3

--- ch2.py
+++ ch2.py
@@ -1,7 +1,6 @@
+"""
 player_health = 1000
 #the best_sword variable holds the value of the best sword in the game !this line is just to learn comments
-
-"""
 posison_damage = -10
 armor_multiplier = 2
 
@@ -71,4 +70,27 @@ game_three_score = 106
 game_four_score = 105
 average_score = (game_one_score + game_two_score + game_three_score + game_four_score) / 4
 print(average_score)
+
+Lesson 19 fixing syntax errors
+name = "Lopen"
+level = 25
+character_class = "Windrunner"
+magic_resistance = 15.0
+account_active = True
+
+# Don't edit below this line
+
+print("Character Report")
+print(f"{name} is a level {level} {character_class}.")
+print(f"They have {magic_resistance} magic resistance.")
+print(f"Their account is currently active: {account_active}")
+
+print("=========================")
+print("Character Report Complete")
+print("Data types:")
+print(
+    f"name: {type(name).__name__}, level: {type(level).__name__}, character_class: {type(character_class).__name__}"
+)
+print(f"magic_resistance: {type(magic_resistance).__name__}")
+print(f"account_active: {type(account_active).__name__}")
 """
