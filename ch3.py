55a1f1248e00f7dfb598bb1a9706d90e2be1903c
RamonCruz-Hinojosa <bairdwins@gmail.com>
2025-10-14 18:39:36 -0500

    ended on L6

--- ch3.py
+++ ch3.py
@@ -52,4 +52,28 @@ second_triple_attack_damage = triple_attack(attack_four, attack_five, attack_six
 print("Getting damage for", attack_four, attack_five, "and", attack_six, "...")
 print(second_triple_attack_damage, "points of damage dealt!")
 print("=====================================")
+
+lesson 5 printing vs returning # changed the print to return in get_title function
+
+def get_title(first_name, last_name, job):
+    title = first_name + " " + last_name + " the " + job
+    return(title)
+
+
+# Don't touch below this line
+
+
+def test(first_name, last_name, job):
+    title = get_title(first_name, last_name, job)
+    print("First name:", first_name)
+    print("Last name:", last_name)
+    print("Job:", job)
+    print("Title:", title)
+    print("=====================================")
+
+
+test("Frodo", "Baggins", "warrior")
+test("Bilbo", "Baggins", "thief")
+test("Gandalf", "The Grey", "wizard")
+test("Aragorn", "Son of Arathorn", "ranger")
 """
